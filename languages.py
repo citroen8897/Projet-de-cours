2bb98dcfd229fb8dd79cc1794e7452a84f10535a
predator2508 <citroen8897@gmail.com>
2020-12-29 12:26:29 +0200

    добавлен французский словарь

--- languages.py
+++ languages.py
@@ -65,30 +65,33 @@ F = ['Choisissez la langue de programme',
      '0 - le mot de passe n"est pas bien. Remplissez autre\n',
      '1 - le mot de passe n"est pas bien. Remplissez autre\n',
      '2 - le mot de passe n"est pas bien. Remplissez autre\n',
-     'Repetez le mot de passe: '
-     'Выберите опцию:\n1 - регистрация нового пользователя\n2 - управление '
-     'учетными записями\n3 - выход\nВаш выбор: ',
-     'Для выхода нажмите Enter',
-     'Пользователь с таким номером уже зарегистрирован!',
-     'В системе зарегистрировано',
-     'пользователей',
-     'Отобразить список пользователей?\nД/н: ',
-     'Д',
-     'н',
-     'Открыть детальную информацию о конкретном пользователе?\nД/н: ',
-     'Некорректный ввод. Программа возвращается в Главное меню.',
-     'Введите порядковый номер пользователя: ',
-     'Порядковый номер не существует! Повторите...',
-     'Данные пользователя:',
-     'Порядковый номер: ',
-     'Телефон: ',
+     'Répétez le mot de passe: ',
+     '\n*** Menu principal ***\nChoisissez une option:\n1 - enregistrement '
+     'd"un nouvel utilisateur\n2 - gestion de compte\n3 - Sortir\n'
+     'Votre choix: ',
+     'Pour sortir cliquez sur Enter',
+     'L"utilisateur avec ce numéro est déjà enregistré!',
+     'Enregistré dans le système',
+     'utilisateurs',
+     'Afficher une liste d"utilisateurs?\nOUI/non: ',
+     'OUI',
+     'non',
+     'Ouvrir des informations détaillées sur un utilisateur spécifique?\n'
+     'OUI/non: ',
+     'Entrée invalide. Le programme revient au menu principal.',
+     'Entrez le numéro de série de l"utilisateur: ',
+     'Le numéro de série n"existe pas! Répétez...',
+     'Information d"utilisateur:',
+     'Le numéro de série: ',
+     'Telephone: ',
      'E-mail: ',
-     'Пароль: '
-     'Открыть опции управления учетной записью?\nД/н: ',
-     'Сбросить пароль пользователя - 4\nУдалить пользователя - 5\nВозврат в '
-     'главное меню - 6\nВаш выбор: ',
-     'Вы действительно желаете удалить пользователя ',
-     'Пользователь удален из системы.',
-     'База данных пуста!']
+     'Le mot de passe: ',
+     'Ouvrir les options de gestion de compte?\nOUI/non: ',
+     'Réinitialiser le mot de passe de l"utilisateur - 4\n'
+     'Supprimer l"utilisateur - 5\nRevenir au menu principal - 6\n'
+     'Votre choix: ',
+     'Voulez-vous vraiment supprimer l"utilisateur ',
+     'L"utilisateur a été supprimé du système.',
+     'La base de données est vide!']
 
 langue_data_base_plus = {'russian': R, 'francaise': F}
